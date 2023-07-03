82dbbbdd1a9d98e6b8f4ec06e8ca6e1443e7477d
Abhishek-Adhikari-1 <119748874+Abhishek-Adhikari-1@users.noreply.github.com>
2023-07-03 21:14:24 +0545

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 const httpServer = require("http").createServer(app);
-const port = process.env.port || 8000;
+const port = process.env.PORT || 8000; // Changed 'port' to 'PORT'
 
 const io = require("socket.io")(httpServer, {
-	cors: {
-		origin: "*",
-		methods: ["GET", "POST"],
-	},
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
 const users = {};
@@ -15,25 +15,25 @@ const users = {};
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-	res.sendFile(__dirname + "index.html");
+  res.sendFile(__dirname + "/index.html"); // Added a forward slash before "index.html"
 });
 
 io.on("connection", (socket) => {
-	socket.on("new-user-connected", (user) => {
-		if (user !== "") {
-			users[socket.id] = user;
-			socket.broadcast.emit("user-connected", user);
-		}
-	});
-	socket.on("message", (msg) => {
-		socket.broadcast.emit("message", msg);
-	});
-	socket.on("disconnecting", (user) => {
-		socket.broadcast.emit("user-disconnected", users[socket.id]);
-		users[socket.id] = null;
-	});
+  socket.on("new-user-connected", (user) => {
+    if (user !== "") {
+      users[socket.id] = user;
+      io.emit("user-connected", user); // Changed 'socket.broadcast.emit' to 'io.emit'
+    }
+  });
+  socket.on("message", (msg) => {
+    io.emit("message", msg); // Changed 'socket.broadcast.emit' to 'io.emit'
+  });
+  socket.on("disconnecting", () => { // Removed unused 'user' parameter
+    io.emit("user-disconnected", users[socket.id]);
+    delete users[socket.id]; // Changed assignment to 'delete' to remove the user from 'users' object
+  });
 });
 
 httpServer.listen(port, () => {
-	console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
